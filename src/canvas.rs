a2512a46408c26aa74f2ea75802d66aba6c4a1fc
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2026-03-30 14:14:32 +0100

    refactor: move block size logic to canvas utils

--- src/canvas.rs
+++ src/canvas.rs
@@ -155,6 +155,60 @@ pub mod canvas_utils {
     pub type CanvasError = Box<dyn std::error::Error + Send + Sync + 'static>;
     pub type Canvas = Vec<Vec<Codel>>;
 
+    mod block_size_utils {
+        use crate::canvas::canvas_utils::Canvas;
+
+        fn fill(canvas: &mut Canvas, x: usize, y: usize, colour_name: &str, counted_block: i64) -> i64 {
+
+            let codel = &mut canvas[x][y];
+            if codel.is_marked() {
+                return counted_block
+            }
+            if !codel.is_colour(colour_name) {
+                return counted_block
+            }
+
+            codel.mark();
+
+            let mut new_count = counted_block + 1;
+            if x > 0 {new_count = fill(canvas, x-1, y, colour_name, new_count);}
+            if y > 0 {new_count = fill(canvas, x, y-1, colour_name, new_count);}
+            if x+1 < canvas.len() {new_count = fill(canvas, x+1, y, colour_name, new_count);}
+            if y+1 < canvas[0].len() {new_count = fill(canvas, x, y+1, colour_name, new_count);}
+
+            return new_count
+        }
+
+        fn get_block_size_and_mark(canvas: &mut Canvas, x: usize, y: usize) -> i64 {
+            let colour_name = canvas[x][y].name();
+            return fill(canvas, x, y, colour_name, 0)
+        }
+
+        fn set_block_size_at_marked(canvas: &mut Canvas, block_size: i64) {
+            for x in 0..canvas.len() {
+                for y in 0..canvas[x].len() {
+                    if canvas[x][y].is_marked() {
+                        canvas[x][y].set_block_size(block_size);
+                    }
+                }
+            }
+        }
+
+        // This algorithm is horribly unoptimised; a better version is Paul Heckbert's span fill. Not a huge worry, as this is a compile-time problem.
+        // Note: this also doesn't cover blocks that span canvases! Implicitly imagines a thin white frame around each canvas.
+        pub fn set_block_sizes(canvas: &mut Canvas) {
+            for x in 0..canvas.len() {
+                for y in 0..canvas[x].len() {
+                    if canvas[x][y].block_size_is_none() {
+                        let block_size = get_block_size_and_mark(canvas, x, y);
+                        set_block_size_at_marked(canvas, block_size);
+                    }
+                }
+            }
+        }
+
+    }
+
     fn codel_from_rgba_and_pos(colour: image::Rgba<u8>, x: usize, y: usize) -> Codel {
         match colour {
             rgba_palette::BLACK => Codel::new(x, y, str_palette::BLACK),
@@ -212,6 +266,9 @@ pub mod canvas_utils {
             }
             canvas.push(row);
         }
+
+        block_size_utils::set_block_sizes(&mut canvas);
+
         Ok(canvas)
     }
 }
