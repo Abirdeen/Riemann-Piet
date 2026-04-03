01d323fa7d978a503fa424fd646d8a99b4bc5c82
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2026-04-03 04:53:52 +0100

    refactor: use blocks instead of codels

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,13 +1,17 @@
 extern crate image;
+extern crate itertools;
+
+use crate::itertools::Itertools;
 use crate::palette;
 
-use crate::str_palette;
+pub type CanvasError = Box<dyn std::error::Error + Send + Sync + 'static>;
+pub type Coordinate = (usize, usize);
 
 #[derive(Debug, Clone, Copy)]
 pub struct Colour {
     hue: u8,
     light: u8,
-    block_size: Option<i64>,
+    block_index: Option<usize>,
     name: &'static str 
 }
 
@@ -19,12 +23,13 @@ impl Colour {
         self.light
     }
     fn block_index(&self) -> Option<usize> {
+        self.block_index
     }
     fn name(&self) -> &'static str {
         self.name
     }
-    fn set_block_size(&mut self, new_size: i64) {
-        self.block_size = Some(new_size);
+    fn set_block_index(&mut self, new_index: usize) {
+        self.block_index = Some(new_index);
         return
     }
 }
@@ -89,28 +94,22 @@ impl Codel {
             _ => None
         }
     }
-
-    pub fn set_block_size(&mut self, new_size: i64) {
+    pub fn block_index(&self) -> Option<usize> {
         match self {
-            Codel::Colour(colour) => colour.set_block_size(new_size),
+            Codel::Colour(colour) => colour.block_index(),
+            _ => None
+        }
+    }
+
+    pub fn set_block_index(&mut self, new_index: usize) {
+        match self {
+            Codel::Colour(colour) => colour.set_block_index(new_index),
             _ => ()
         }
     }
-
-    pub fn mark(&mut self) {
-        self.set_block_size(-1);
-    }
-
-    pub fn is_marked(&self) -> bool {
+    pub fn no_index(&self) -> bool {
         match self {
-            Codel::Colour(colour) if colour.block_size() == Some(-1) => true,
-            _ => false
-        }
-    }
-
-    pub fn block_size_is_none(&self) -> bool {
-        match self {
-            Codel::Colour(colour) if colour.block_size() == None => true,
+            Codel::Colour(colour) if colour.block_index() == None => true,
             _ => false
         }
     }
@@ -123,11 +122,196 @@ impl Codel {
             _ => false
         }
     }
+#[derive(Debug, Clone)]
+pub struct CodelBlock {
+    index: usize,
+    northmost_west_coord: Coordinate,
+    northmost_east_coord: Coordinate,
+    eastmost_north_coord: Coordinate,
+    eastmost_south_coord: Coordinate,
+    southmost_east_coord: Coordinate,
+    southmost_west_coord: Coordinate,
+    westmost_south_coord: Coordinate,
+    westmost_north_coord: Coordinate,
+    block_size: u32
+}
+
+impl CodelBlock {
+    pub fn new(index: usize, coordinate: Coordinate) -> CodelBlock {
+        CodelBlock { index, 
+            northmost_west_coord: coordinate, 
+            northmost_east_coord: coordinate, 
+            eastmost_north_coord: coordinate, 
+            eastmost_south_coord: coordinate, 
+            southmost_east_coord: coordinate, 
+            southmost_west_coord: coordinate, 
+            westmost_south_coord: coordinate, 
+            westmost_north_coord: coordinate, 
+            block_size: 0 }
+    }
+
+    pub fn index(&self) -> usize {
+        self.index
+    }
+
+    pub fn northmost_west(&self) -> Coordinate {
+        self.northmost_west_coord
+    }
+    pub fn northmost_east(&self) -> Coordinate {
+        self.northmost_east_coord
+    }
+    pub fn eastmost_north(&self) -> Coordinate {
+        self.eastmost_north_coord
+    }
+    pub fn eastmost_south(&self) -> Coordinate {
+        self.eastmost_south_coord
+    }
+    pub fn southmost_east(&self) -> Coordinate {
+        self.southmost_east_coord
+    }
+    pub fn southmost_west(&self) -> Coordinate {
+        self.southmost_west_coord
+    }
+    pub fn westmost_south(&self) -> Coordinate {
+        self.westmost_south_coord
+    }
+    pub fn westmost_north(&self) -> Coordinate {
+        self.westmost_north_coord
+    }
+    pub fn size(&self) -> u32 {
+        self.block_size
+    }
+
+    fn update_northmost(&mut self, coordinate: Coordinate) {
+        let (nwx, nwy) = self.northmost_west();
+        let (nex, _) = self.northmost_east();
+        match coordinate {
+            (_,y) if y > nwy => (),
+            (_,y) if y < nwy => {
+                self.northmost_west_coord = coordinate;
+                self.northmost_east_coord = coordinate;
+            },
+            (x,_) if x < nwx => self.northmost_west_coord = coordinate,
+            (x,_) if x > nex => self.northmost_east_coord = coordinate,
+            _ => ()
+        }
+    }
+    fn update_eastmost(&mut self, coordinate: Coordinate) {
+        let (enx, eny) = self.eastmost_north();
+        let (_, esy) = self.eastmost_south();
+        match coordinate {
+            (x,_) if x < enx => (),
+            (x,_) if x > enx => {
+                self.eastmost_north_coord = coordinate;
+                self.eastmost_south_coord = coordinate;
+            },
+            (_,y) if y < eny => self.eastmost_north_coord = coordinate,
+            (_,y) if y > esy => self.eastmost_south_coord = coordinate,
+            _ => ()
+        }
+    }
+    fn update_southmost(&mut self, coordinate: Coordinate) {
+        let (swx, swy) = self.southmost_west();
+        let (sex, _) = self.southmost_east();
+        match coordinate {
+            (_,y) if y < swy => (),
+            (_,y) if y > swy => {
+                self.southmost_west_coord = coordinate;
+                self.southmost_east_coord = coordinate;
+            },
+            (x,_) if x < swx => self.southmost_west_coord = coordinate,
+            (x,_) if x > sex => self.southmost_east_coord = coordinate,
+            _ => ()
+        }
+    }
+    fn update_westmost(&mut self, coordinate: Coordinate) {
+        let (wnx, wny) = self.westmost_north();
+        let (_, wsy) = self.westmost_south();
+        match coordinate {
+            (x,_) if x > wnx => (),
+            (x,_) if x < wnx => {
+                self.westmost_north_coord = coordinate;
+                self.westmost_south_coord = coordinate;
+            },
+            (_,y) if y < wny => self.westmost_north_coord = coordinate,
+            (_,y) if y > wsy => self.westmost_south_coord = coordinate,
+            _ => ()
+        }
+    }
+
+    pub fn add_coordinate(&mut self, coordinate: Coordinate) {
+        self.block_size += 1;
+        self.update_northmost(coordinate);
+        self.update_eastmost(coordinate);
+        self.update_southmost(coordinate);
+        self.update_westmost(coordinate);
+    }
 
 }
 
-pub type CanvasError = Box<dyn std::error::Error + Send + Sync + 'static>;
-pub type Canvas = Vec<Vec<Codel>>;
+#[derive(Debug, Clone)]
+pub struct Canvas {
+    codel_map: Vec<Vec<Codel>>,
+    blocks: Vec<CodelBlock>,
+    dimensions: (usize, usize),
+    codel_iter: itertools::Product<std::ops::Range<usize>, std::ops::Range<usize>>
+}
+
+impl Canvas {
+    pub fn new(codel_map: Vec<Vec<Codel>>) -> Box<Canvas> {
+        let width = codel_map.len();
+        let height = codel_map[0].len();
+        let codel_iter: itertools::Product<std::ops::Range<usize>, std::ops::Range<usize>> = (0..width).cartesian_product(0..height);
+        let blocks = Vec::new();
+        Box::new(Canvas { codel_map, blocks, dimensions: (width, height), codel_iter })
+    }
+
+    pub fn dimensions(&self) -> (usize, usize) {
+        self.dimensions
+    }
+    pub fn coordinates_iter(&self) -> itertools::Product<std::ops::Range<usize>, std::ops::Range<usize>> {
+        self.codel_iter.clone()
+    }
+
+    pub fn get_codel(&mut self, coordinate: Coordinate) -> &mut Codel {
+        let (x, y) = coordinate;
+        &mut self.codel_map[x][y]
+    }
+    pub fn get_block_from_coord(&mut self, coordinate: Coordinate) -> Option<&mut CodelBlock> {
+        let index = self.get_codel(coordinate).block_index()?;
+        return Some(&mut self.blocks[index])
+    }
+
+    pub fn north(&self, (x,y): Coordinate) -> Coordinate {
+        return (x, y-1)
+    }
+    pub fn east(&self, (x,y): Coordinate) -> Coordinate {
+        return (x+1, y)
+    }
+    pub fn south(&self, (x,y): Coordinate) -> Coordinate {
+        return (x, y+1)
+    }
+    pub fn west(&self, (x,y): Coordinate) -> Coordinate {
+        return (x-1, y)
+    }
+
+    pub fn is_northmost(&self, (_,y): Coordinate) -> bool {
+        return y==0
+    }
+    pub fn is_eastmost(&self, (x,_): Coordinate) -> bool {
+        return x==((self.dimensions().0) - 1)
+    }
+    pub fn is_southmost(&self, (_,y): Coordinate) -> bool {
+        return y==((self.dimensions().1) - 1)
+    }
+    pub fn is_westmost(&self, (x,_): Coordinate) -> bool {
+        return x==0
+    }
+
+    pub fn add_block(&mut self, block: CodelBlock) {
+        self.blocks.push(block);
+    }
+}
 
 mod canvas_utils {
     use crate::{Canvas, Codel};
@@ -137,52 +321,59 @@ mod canvas_utils {
 
     mod block_utils {
         use crate::Canvas;
+        use crate::canvas::{CodelBlock, Coordinate};
 
-        fn fill(canvas: &mut Canvas, x: usize, y: usize, colour_name: &str, counted_block: i64) -> i64 {
+        fn try_cardinal_fills(
+            canvas: &mut Canvas, 
+            coordinate: Coordinate, 
+            colour_name: &str, 
+            block: &mut CodelBlock
+        ) {
+            if !canvas.is_northmost(coordinate) {
+                build_block(canvas, canvas.north(coordinate), colour_name, block);
+            };
+            if !canvas.is_eastmost(coordinate) {
+                build_block(canvas, canvas.east(coordinate), colour_name, block);
+            };
+            if !canvas.is_southmost(coordinate) {
+                build_block(canvas, canvas.south(coordinate), colour_name, block);
+            };
+            if !canvas.is_westmost(coordinate) {
+                build_block(canvas, canvas.west(coordinate), colour_name, block);
+            };
+        }
 
-            let codel = &mut canvas[x][y];
-            if codel.is_marked() {
-                return counted_block
+        fn build_block(
+            canvas: &mut Canvas, 
+            coordinate: Coordinate, 
+            colour_name: &str, 
+            block: &mut CodelBlock
+        ) {
+            let codel = canvas.get_codel(coordinate);
+            if !codel.no_index() {
+                return ()
             }
             if !codel.is_colour(colour_name) {
-                return counted_block
+                return ()
             }
 
-            codel.mark();
+            block.add_coordinate(coordinate);
+            codel.set_block_index(block.index());
 
-            let mut new_count = counted_block + 1;
-            if x > 0 {new_count = fill(canvas, x-1, y, colour_name, new_count);}
-            if y > 0 {new_count = fill(canvas, x, y-1, colour_name, new_count);}
-            if x+1 < canvas.len() {new_count = fill(canvas, x+1, y, colour_name, new_count);}
-            if y+1 < canvas[0].len() {new_count = fill(canvas, x, y+1, colour_name, new_count);}
-
-            return new_count
-        }
-
-        fn get_block_size_and_mark(canvas: &mut Canvas, x: usize, y: usize) -> i64 {
-            let colour_name = canvas[x][y].name();
-            return fill(canvas, x, y, colour_name, 0)
-        }
-
-        fn set_block_size_at_marked(canvas: &mut Canvas, block_size: i64) {
-            for x in 0..canvas.len() {
-                for y in 0..canvas[x].len() {
-                    if canvas[x][y].is_marked() {
-                        canvas[x][y].set_block_size(block_size);
-                    }
-                }
-            }
+            try_cardinal_fills(canvas, coordinate, colour_name, block)
         }
 
         // This algorithm is horribly unoptimised; a better version is Paul Heckbert's span fill. Not a huge worry, as this is a compile-time problem.
         // Note: this also doesn't cover blocks that span canvases! Implicitly imagines a thin white frame around each canvas.
         pub fn set_block_sizes(canvas: &mut Canvas) {
-            for x in 0..canvas.len() {
-                for y in 0..canvas[x].len() {
-                    if canvas[x][y].block_size_is_none() {
-                        let block_size = get_block_size_and_mark(canvas, x, y);
-                        set_block_size_at_marked(canvas, block_size);
-                    }
+            let mut index = 0;
+            for coordinate in canvas.coordinates_iter() {
+                if canvas.get_codel(coordinate).no_index() {
+                    let mut block = CodelBlock::new(index, coordinate);
+                    let colour_name = canvas.get_codel(coordinate).name();
+                    build_block(canvas, coordinate, colour_name, &mut block);
+                    canvas.add_block(block);
+                    index += 1;
                 }
             }
         }
@@ -240,7 +431,7 @@ mod canvas_utils {
         let image = image::open(img_path)?;
         let (width, height) = scaled_dimensions(&image, codel_size)?;
 
-        let mut canvas: Canvas = Vec::new();
+        let mut codel_map: Vec<Vec<Codel>> = Vec::new();
         for i in 0..width {
             let mut row: Vec<Codel> = Vec::new();
             for j in 0..height {
@@ -249,11 +440,12 @@ mod canvas_utils {
                     );
                 row.push(codel);
             }
-            canvas.push(row);
+            codel_map.push(row);
         }
 
-        block_size_utils::set_block_sizes(&mut canvas);
+        let mut canvas = *Canvas::new(codel_map);
 
+        block_utils::set_block_sizes(&mut canvas);
 
         Ok(canvas)
     }
